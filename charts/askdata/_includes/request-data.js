b380f5ab9d3c86729f621d1653cfe159667472cb
Askdata <info@askdata.com>
2020-11-07 20:24:13 +0100

    Refactoring with includes

---
+++ charts/askdata/_includes/request-data.js
@@ -0,0 +1,43 @@
+// Retrieve auth token
+if (getUrlParameter('token') != undefined){
+  var token = getUrlParameter('token');
+}else{
+  var token = Cookies.get('access_token')
+}
+
+var theme = []
+
+// Retrieve theme
+if (getUrlParameter('theme')) {
+
+    $.getJSON( "https://chart-dev.askdata.com/charts/immuni-app/catalog.json", function( data ) {
+      
+      console.log("retrieving theme")
+      console.log(data)
+      
+      window.theme = data;
+
+  })
+
+}
+
+// Request charting data
+$.ajax({
+         url: environment + "/charts/" + getUrlParameter('chartId'),
+         type: "GET",
+         beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer ' + token 
+          ).setRequestHeader('Content-Type','application/json')},
+         success: function(data) { 
+
+         displayChart(data);
+
+}
+});  
+
+// Redirect to login if not authenticated 
+$( document ).ajaxError(function( event, jqxhr, settings, exception ) {
+    if ( jqxhr.status== 401 ) {
+      location.replace("https://app-dev.askdata.com/login");
+
+    }
+});
